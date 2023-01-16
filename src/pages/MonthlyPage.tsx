0f86477fa17529475be58dfb7c1ce083997f342d
khaledr <info@khaledr.ir>
2023-01-16 16:10:28 +0330

    review

--- src/pages/MonthlyPage.tsx
+++ src/pages/MonthlyPage.tsx
@@ -3,5 +3,5 @@ import React from "react";
 import classes from "./TodayPage.module.scss";
 
 export const MonthlyPage: React.FunctionComponent = () => {
-  return <h3>Monthly Page</h3>;
+  return <h3>Monthly Page, Is ready now</h3>;
 };
